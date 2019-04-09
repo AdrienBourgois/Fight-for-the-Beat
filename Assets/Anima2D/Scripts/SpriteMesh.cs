22cec78fd652c3bf28ac0d73d50887ddc9002a15
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-09 11:26:59 +0200

    Add Anima2D

---
+++ Assets/Anima2D/Scripts/SpriteMesh.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+#pragma warning disable CS0649
+
+namespace Anima2D
+{
+	public class SpriteMesh : ScriptableObject
+	{
+		public const int api_version = 4;
+
+		[SerializeField][HideInInspector]
+		private int m_ApiVersion;
+		[SerializeField]
+		private Sprite m_Sprite;
+		[SerializeField]
+		private Mesh m_SharedMesh;
+
+		public Sprite sprite
+		{
+			get { return m_Sprite; }
+		}
+
+		public Mesh sharedMesh
+		{
+			get { return m_SharedMesh; }
+		}
+	}
+}
+
+#pragma warning restore CS0649
---
+++ Assets/Scripts/Audio/SoundOneShot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using AOT;
+using FMOD;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+namespace Audio
+{
+    public class SoundOneShot : MonoBehaviour
+    {
+
+
+        private EVENT_CALLBACK endCallback;
+        private EventInstance soundInstance;
+        private bool mustBeDestroyed;
+
+        public static void PlayOneShot(string _event, Vector2 _position, float _volume = 1f)
+        {
+            GameObject go = new GameObject("Sound " + _event);
+            SoundOneShot sound = go.AddComponent<SoundOneShot>();
+            sound.SetParametersAndPlay(_event, _volume/* * MusicManager.SoundsVolume*/, _position);
+        }
+
+        public static void PlayOneShot(string _event, float _volume = 1f)
+        {
+            GameObject go = new GameObject("Sound " + _event);
+            SoundOneShot sound = go.AddComponent<SoundOneShot>();
+            sound.SetParametersAndPlay(_event, _volume /** MusicManager.SoundsVolume*/);
+        }
+
+        private void SetParametersAndPlay(string _event, float _volume, Vector2 _position)
+        {
+            endCallback = OnStopped;
+
+            soundInstance = RuntimeManager.CreateInstance(_event);
+
+            Vector3 position = _position;
+            if (Camera.main != null)
+                position.z = Camera.main.transform.position.z + 3f;
+            transform.position = position;
+
+            soundInstance.set3DAttributes(transform.To3DAttributes());
+            soundInstance.setVolume(_volume);
+            soundInstance.setCallback(endCallback, EVENT_CALLBACK_TYPE.STOPPED);
+            soundInstance.start();
+
+        }
+
+        private void SetParametersAndPlay(string _event, float _volume)
+        {
+            endCallback = OnStopped;
+
+            soundInstance = RuntimeManager.CreateInstance(_event);
+
+            transform.position = Vector3.zero;
+
+            soundInstance.setVolume(_volume);
+            soundInstance.setCallback(endCallback, EVENT_CALLBACK_TYPE.STOPPED);
+            soundInstance.start();
+        }
+
+        private void Update()
+        {
+            if(mustBeDestroyed)
+                Destroy(gameObject);
+        }
+
+        [MonoPInvokeCallback(typeof(EVENT_CALLBACK))]
+        private RESULT OnStopped(EVENT_CALLBACK_TYPE _type, EventInstance _eventInstance, IntPtr _parameters)
+        {
+            soundInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            soundInstance.release();
+            mustBeDestroyed = true;
+            return RESULT.OK;
+        }
+    }
+}
