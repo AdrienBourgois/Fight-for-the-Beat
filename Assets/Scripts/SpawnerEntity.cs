8cb8e9331c415b7dbb33a20c3e8ebb31599e9e4e
Adrien Monteiro <a.monteiro@student.isartdigital.com>
2019-04-10 20:03:45 +0200

    Add Spawner

---
+++ Assets/Scripts/SpawnerEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEntity : MonoBehaviour
+{
+    public Entities.Entity PrefabEntity;
+    
+    public int EnemySpaceSize;
+    public int EnemySpacePosition;
+
+    void Start()
+    {
+        Entities.Entity newEntity = Instantiate<Entities.Entity>(PrefabEntity,transform);
+        newEntity.SetSpaceIndex(EnemySpacePosition, EnemySpaceSize);
+    }
+}
