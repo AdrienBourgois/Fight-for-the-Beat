5d37eb6416163eda3891d2cbec7966debb9fa3ae
Adrien Monteiro <a.monteiro@student.isartdigital.com>
2019-04-10 11:50:05 +0200

    Add Sequence Script and SO

---
+++ Assets/Scripts/Entities/Sequence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities
+{
+    [CreateAssetMenu(menuName = "Sequence")]
+    public class Sequence : ScriptableObject
+    {
+        public List<Action> Actions;
+    }
+}
