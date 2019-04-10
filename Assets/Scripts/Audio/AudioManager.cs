f49044b66e733d15985d80c43f577fc506932a75
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-10 11:38:55 +0200

    Inputs - Beat System

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -8,8 +8,38 @@ namespace Audio
     {
         public static AudioManager Instance { get; private set; }
 
+        public const int Bpm = 120;
+        public const int BeatCount = 4;
+
+        public delegate void BeatEvent(int _beat);
+
+        public event BeatEvent OnBeat;
+        public event BeatEvent OnOffbeat;
+
         public float MusicVolume { get; private set; } = 1f;
         public float FxVolume { get; private set; } = 1f;
+
+        public int Beat
+        {
+            get { return beat; }
+            set
+            {
+                if (beat != value)
+                    OnBeat?.Invoke(value);
+                beat = value;
+            }
+        }
+
+        public int Offbeat
+        {
+            get { return offbeat; }
+            set
+            {
+                if (offbeat != value)
+                    OnOffbeat?.Invoke(value);
+                offbeat = value;
+            }
+        }
 
         [SerializeField]
         [EventRef]
@@ -22,6 +52,8 @@ namespace Audio
         private EventInstance eventInstance;
 
         private int beat;
+        private int offbeat;
+        private bool updateBeat = false;
 
         public void SetMusicVolume(float _volume)
         {
@@ -41,16 +73,19 @@ namespace Audio
         {
             GameManager.Instance.OnMenu += OnMenu;
             GameManager.Instance.OnPlay += OnPlay;
+            GameManager.Instance.OnGameOver += () => updateBeat = false;
         }
 
         private void OnPlay()
         {
             SetInstance(musicEvent);
+            updateBeat = true;
         }
 
         private void OnMenu()
         {
             SetInstance(menuEvent);
+            updateBeat = false;
         }
 
         private void SetInstance(string _event)
@@ -64,10 +99,14 @@ namespace Audio
 
         private void Update()
         {
+            if (!updateBeat)
+                return;
+
             eventInstance.getTimelinePosition(out int position);
             float time = position / 1000f;
-            int total_beat = (int)(time / (60f / 120f));
-            beat = total_beat % 4 + 1;
+            float total_beat = time / (60f / Bpm);
+            Beat = (int)total_beat % BeatCount + 1;
+            Offbeat = (int)(total_beat + 60f / Bpm) % BeatCount + 1;
         }
     }
 }
---
+++ Assets/Scripts/GameInputs/InputManager.cs
@@ -0,0 +1,73 @@
+﻿using Audio;
+using UnityEngine;
+
+namespace GameInputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        public enum Keys
+        {
+            Up,
+            Left,
+            Down,
+            Right
+        }
+
+        public delegate void KeyPressedEvent(Keys _key);
+        public delegate void WrongInputEvent();
+
+        public static InputManager Instance { get; private set; }
+
+        public event KeyPressedEvent OnKeyPressed;
+        public event WrongInputEvent OnNoKeyPressed;
+        public event WrongInputEvent OnMultipleKeyPressed;
+
+        private bool canInteract;
+        private bool hadInteract;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            AudioManager.Instance.OnOffbeat += OnOffbeat;
+        }
+
+        private void Update()
+        {
+            if(Input.GetButtonDown("Left"))
+                ProcessInput(Keys.Left);
+            if (Input.GetButtonDown("Right"))
+                ProcessInput(Keys.Right);
+            if (Input.GetButtonDown("Up"))
+                ProcessInput(Keys.Up);
+            if (Input.GetButtonDown("Down"))
+                ProcessInput(Keys.Down);
+        }
+
+        private void OnOffbeat(int _beat)
+        {
+            if(!hadInteract)
+                OnNoKeyPressed?.Invoke();
+
+            canInteract = true;
+            hadInteract = false;
+        }
+
+        private void ProcessInput(Keys _key)
+        {
+            if (hadInteract)
+            {
+                OnMultipleKeyPressed?.Invoke();
+            }
+            else if (canInteract)
+            {
+                canInteract = false;
+                hadInteract = true;
+                OnKeyPressed?.Invoke(_key);
+            }
+        }
+    }
+}
