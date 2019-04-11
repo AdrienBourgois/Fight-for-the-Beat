aa6812633d931d7b3f1f37624be621fb04ba9587
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-11 17:48:45 +0200

    Activators

--- Assets/Scripts/SpawnerEntity.cs
+++ Assets/Scripts/SpawnerEntity.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class SpawnerEntity : MonoBehaviour
 {
     public Entities.Entity PrefabEntity;
-    
+
     public int EnemySpaceSize;
     public int EnemySpacePosition;
-    
+
+    public int SpaceActivator;
+
     public void Spawn()
     {
         Entities.Entity newEntity = Instantiate<Entities.Entity>(PrefabEntity, transform.position, Quaternion.identity);
         newEntity.SetSpaceIndex(EnemySpacePosition, EnemySpaceSize);
-        
+
+        gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnPlayerPlayed += DetectPlayer;
+    }
+
+    private void DetectPlayer()
+    {
+        Entities.Entity entity = Level.SpaceManager.Instance.GetEntityOnSpace(SpaceActivator);
+        if (entity)
+        {
+            if (entity as Entities.Player)
+                ActiveTarget();
+        }
+    }
+
+    private void ActiveTarget()
+    {
+        Spawn();
+
+        GameManager.Instance.OnPlayerPlayed -= DetectPlayer;
         gameObject.SetActive(false);
     }
 }
