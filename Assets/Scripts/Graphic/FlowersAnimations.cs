63a72cbe43efc555da0a4e375ee7649037ecc853
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-11 20:25:16 +0200

    Background

---
+++ Assets/Scripts/Graphic/FlowersAnimations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Graphic
+{
+    public class FlowersAnimations : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<Animator>().Play("Fleur", -1, Random.value);
+        }
+    }
+}
