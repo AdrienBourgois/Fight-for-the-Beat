f4e721a5bb37493059412915bb0a05a510628beb
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-11 18:31:34 +0200

    Banks - Camera

---
+++ Assets/Scripts/Graphic/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Graphic
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        #pragma warning disable CS0649
+        [SerializeField]
+        private GameObject target;
+        #pragma warning restore CS0649
+
+        private float delta;
+
+        private void Start()
+        {
+            delta = transform.position.x;
+        }
+
+        private void Update()
+        {
+            Transform object_transform = transform;
+            Vector3 position = object_transform.position;
+            position.x = target.transform.position.x + delta;
+            object_transform.position = position;
+        }
+    }
+}
