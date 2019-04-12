5aec6581078b2f5d7b217801b753e46be83c0f83
AdrienBourgois <a.bourgois@student.isartdigital.com>
2019-04-12 12:42:18 +0200

    Sounds

---
+++ Assets/Scripts/Audio/EnemiesSounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class EnemiesSounds : MonoBehaviour
+    {
+        public void PlayFlowerAttackEvent() => AudioManager.Instance.PlayFlowerAttackEvent();
+        public void PlayFlowerFootstepEvent() => AudioManager.Instance.PlayFlowerFootstepEvent();
+        public void PlayFlowerPrepaEvent() => AudioManager.Instance.PlayFlowerPrepaEvent();
+        public void PlayFouetAttack() => AudioManager.Instance.PlayFouetAttack();
+        public void PlayFouetJump() => AudioManager.Instance.PlayFouetJump();
+        public void PlayHatAttackEvent() => AudioManager.Instance.PlayHatAttackEvent();
+        public void PlayHatPrepaEvent() => AudioManager.Instance.PlayHatPrepaEvent();
+        public void PlayPuppetAttackEvent() => AudioManager.Instance.PlayPuppetAttackEvent();
+        public void PlayPuppetPrepaEvent() => AudioManager.Instance.PlayPuppetPrepaEvent();
+    }
+}
